af1d79d98b77b3f09e5ba65160e3f610f9014626
John Assefa <johnassefaforbussines@gmail.com>
2023-12-28 06:12:14 -0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
-
-// HTTP connection to the API
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'https://countries.trevorblades.com/',
-})
-
-// Cache implementation
-const cache = new InMemoryCache()
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-})
-</script>
-
 <template>
   <div>
-    
-    
+    <h1>Countries</h1>
+    <ul v-if="!loading && !error">
+      <li v-for="country in countries" :key="country.code">
+        {{ country.name }} - {{ country.capital }} ({{ country.code }})
+      </li>
+    </ul>
+    <p v-if="loading">Loading...</p>
+    <p v-if="error">Error loading data</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { onMounted, ref } from 'vue';
+import { gql } from '@apollo/client/core';
+import { useQuery } from '@vue/apollo-composable';
+
+const countriesQuery = gql`
+  query {
+    countries {
+      name
+      code
+      capital
+    }
+  }
+`;
+
+const { result, loading, error } = useQuery(countriesQuery);
+const countries = ref([]);
+
+onMounted(async () => {
+  console.log(result)
+  const data  =result.value; // Access the function by calling result.value()
+  countries.value = data.countries;
+
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp, provide, h } from 'vue';
+import App from './App.vue'; // Make sure to provide the correct path to your App component
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+  uri: 'https://countries.trevorblades.com/',
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
+
+app.mount('#app');
