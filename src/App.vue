e48dc35026eda6877399968089e32439438f591a
John Assefa <johnassefaforbussines@gmail.com>
2023-12-28 00:42:10 -0800

    intializing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
 </script>
 
 <template>
